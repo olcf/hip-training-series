b1d38036d38b8ac4e2ae09717235315e7566ca5d
Bob Robey <Bob.Robey@amd.com>
2023-08-24 19:50:20 -0500

    Adding hipify scripts

---
+++ Lecture2/HIPIFY/hipexamine-perl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#usage : hipexamine-perl.sh DIRNAME [hipify-perl options]
+
+# Generate HIP stats (LOC, CUDA->API conversions, missing functionality) for all the code files
+# in the specified directory.
+
+
+SCRIPT_DIR=`dirname $0`
+PRIV_SCRIPT_DIR="$SCRIPT_DIR/../libexec/hipify"
+SEARCH_DIR=$1
+shift
+$SCRIPT_DIR/hipify-perl -no-output -print-stats "$@" `$PRIV_SCRIPT_DIR/findcode.sh $SEARCH_DIR`
