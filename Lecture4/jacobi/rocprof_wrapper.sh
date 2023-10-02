9680c709dd132ba2d27b4bd63972e55985736979
Bob Robey <Bob.Robey@amd.com>
2023-10-02 10:25:36 -0500

    Updating rocprof wrapper

--- Lecture4/jacobi/rocprof_wrapper.sh
+++ Lecture4/jacobi/rocprof_wrapper.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 set -euo pipefail
-# depends on ROCM_PATH being set outside;  input arguments are the output directory & the name
-outdir="$1"
-name="$2"
-if [[ -n ${OMPI_COMM_WORLD_RANK+z} ]]; then
-    # mpich
-    export MPI_RANK=${OMPI_COMM_WORLD_RANK}
-elif [[ -n ${MV2_COMM_WORLD_RANK+z} ]]; then
-    # ompi
-    export MPI_RANK=${MV2_COMM_WORLD_RANK}
-elif [[ -n ${SLURM_PROCID+z} ]]; then
-    export MPI_RANK=${SLURM_PROCID}
-else
-    echo "Unknown MPI layer detected! Must use OpenMPI, MVAPICH, or SLURM"
-    exit 1
-fi
-rocprof="${ROCM_PATH}/bin/rocprof"
-
-pid="$$"
-outdir="${outdir}/rank_${pid}_${MPI_RANK}"
-outfile="${name}_${pid}_${MPI_RANK}.csv"
-${rocprof} -d ${outdir} --hsa-trace -o ${outdir}/${outfile} "${@:3}"
-
+outdir=$1
+name=$2
+outdir="${outdir}_${OMPI_COMM_WORLD_RANK}"
+outfile="${name}_${OMPI_COMM_WORLD_RANK}.csv"
+rocprof -d ${outdir} --hsa-trace --hip-trace -o ${outdir}/${outfile} ./Jacobi_hip $3 $4
